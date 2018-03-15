61a3282e383a6e230e14a0d15d8b3ebb1bdafd57
Mikhail <12schneide@gmail.com>
2018-03-14 21:56:39 -0400

    Updated C++ version to have mutlithreading and a standard naming convention

--- SieveOfEranthoses.cpp
+++ SieveOfEranthoses.cpp
@@ -2,37 +2,65 @@
 #include <string>
 #include <vector>
 #include <fstream>
+#include <thread>
+#include <atomic>
 
-#define PRINT_STATUS 0
+typedef unsigned long long int uint64;
 
-using uint64 = unsigned long long int;
 
-int main() {
-	std::cout << "Enter the amount to generate: ";
-	uint64 genTo = 10;
-	uint64 minPrime = 0;
 
-	std::string inStr = "10";
-	while(![&]()->bool{
-		std::getline(std::cin, inStr);
+std::atomic<uint64> GenTo(0);
+std::atomic<uint64> MinPrime(0);
+std::atomic<uint64> CurIndex(0);
+std::atomic<bool> Working(false);
+
+void ProgressFunction();
+
+
+
+int main(){
+
+    std::ios::sync_with_stdio(false);
+
+    //=========================
+
+    std::vector<bool> Sieve;
+
+    std::string InStr("");
+
+    std::thread* ProgressThread;
+
+
+    //=========================
+
+
+    std::cout << "Enter the amount to generate: " ;
+
+    InStr = "10";
+    while(![&]()-> bool{
+        std::getline(std::cin, InStr);
 		try {
-			genTo = std::stoull(inStr);
+			GenTo = std::stoull(InStr);
 			return true;
 		}
 		catch (...) {
 			std::cout << "Enter an interger." << std::endl;
 			return false;
 		}
-	}());
+    }());
 
-	std::cout << "Enter a minimium prime: ";
+    std::cout << "Allocating sieve..." << std::endl;
 
-	inStr = "0";
+    Sieve.resize(GenTo);
+
+    std::cout << "Enter a minium prime: ";
+
+    InStr = "0";
 	while(![&]()->bool{
-		std::getline(std::cin, inStr);
+		std::getline(std::cin, InStr);
 		try {
-			minPrime = std::stoull(inStr);
-			if (minPrime >= genTo) {
+			MinPrime = std::stoull(InStr);
+			if (MinPrime >= GenTo) {
 				std::cout << "Minimium must be below generate to." << std::endl;
 				return false;
 			}
@@ -46,39 +74,83 @@ int main() {
 		}
 	}());
 
-	std::cout << "Generating..." << std::endl;
-	std::vector<bool> primes;
-	[&]() -> void
-	{
-		primes.resize(genTo);
-		for (uint64 i = 2; i < primes.size(); i++) {
-			if (!primes[i]) {
-				for (uint64 r = i + i; r < primes.size(); r += i) {
-					primes[r] = true;
-#if PRINT_STATUS
-					printf("Marking off index:  %i\n", r);
-#endif
-				}
+    std::cout << "Generating..." << std::endl;
+
+    Working = true;
+
+    ProgressThread = new std::thread(ProgressFunction);
+ 
+    CurIndex = 1;
+
+    while(++CurIndex < GenTo){
+        if (!Sieve[CurIndex]) {
+			for (uint64 r = CurIndex + CurIndex; r < GenTo; r += CurIndex) {
+				Sieve[r] = true;
 			}
 		}
-		return;
-	}();
+    }
 
-	std::string outputFileName = "GeneratedPrimesTo" + std::to_string(genTo) + ".txt";
+    /*for (uint64 i = 2; i < Sieve.size(); i++) {
+		if (!Sieve[i]) {
+			for (uint64 r = i + i; r < Sieve.size(); r += i) {
+				Sieve[r] = true;
+			}
+		}
+		CurIndex = i;
+	}*/
 
-	std::ofstream outputFile;
-	outputFile.open(outputFileName);
+    Working = false;
+
+    ProgressThread->join();
+
+    delete ProgressThread;
+
+	std::string OutputFileName = "GeneratedPrimesFrom" + std::to_string(MinPrime) + "To" + std::to_string(GenTo) + ".txt";
+
+	std::ofstream OutputFile;
+	OutputFile.open(OutputFileName);
 
 	std::cout << "Writing to file..." << std::endl;
 
-	for (uint64 i = 0; i < primes.size(); i++) {
-#if PRINT_STATUS
-		printf("Index: %i is %i\n", i, primes[i]);
-#endif
-		if (!primes[i] && i >= minPrime) {
-			outputFile << i << '\n';
-		}
-	}
+    Working = true;
 
-	printf("Done\n");
+    ProgressThread = new std::thread(ProgressFunction);
+
+    CurIndex = static_cast<uint64>(MinPrime);
+
+    while(++CurIndex < GenTo){
+        if (!Sieve[CurIndex] && CurIndex >= MinPrime) {
+			OutputFile << CurIndex << '\n';
+		}
+    }
+
+    /*for (uint64 i = 0; i < Sieve.size(); i++) {
+		if (!Sieve[i] && i >= MinPrime) {
+			OutputFile << i << '\n';
+		}
+        CurIndex = i;
+	}*/
+
+    Working = false;
+
+    ProgressThread->join();
+
+    delete ProgressThread;
+
+    std::cout << "Done" << std::endl;
+
+    return 0;
+
+}
+
+void ProgressFunction(){
+
+    std::cout << "                                 \r";
+
+    while(Working){
+        std::cout << "Progress: " << (static_cast<float>(CurIndex) / static_cast<float>(GenTo) * 100.f) << "%                       \r";
+    }
+
+    std::cout << "                                  \r";
+
 }
