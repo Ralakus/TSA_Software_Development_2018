be327eb60b6de51bfc1845426fb0b51ccbcb33b7
Heidel <heidelzb48@CAMPUS.LCPS>
2018-04-11 17:42:28 -0400

    mess

--- src/Main.java
+++ src/Main.java
@@ -2,96 +2,198 @@ import java.util.*;
 public class Main {
 
 	static Scanner scan = new Scanner(System.in);
-	private static ArrayList<Alternative> alternatives = new ArrayList<Alternative>();
-	private static HashMap<String, Integer> criteria = new HashMap<String, Integer>();
+	static ArrayList<User> users = new ArrayList<User>();
+	
+	
 	
 	
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+
+		initialize();
+		makeUsers();
 		getAlternatives();
-		getCriteriaAndWeights();
-		getAlternativeSuccess();
-		multiply();
+		int counter = 0;
+		for(User user: users)
+		{
+			if(users.size() > 1)
+			{
+				userMessage(counter);
+			}
+			getCriteriaAndWeights(user);
+			getAlternativeSuccess(user);
+			multiply(user);
+			counter++;
+		}
+		if(users.size() > 1)
+		{
+			getGroupDecision();
+		}
 		
+	}
+	
+	private static void userMessage(int num)
+	{
+		space();
+		if(num > 0)
+		{
+			System.out.println("Group member " + num + " has completed their part.");
+			readLine("Please switch to the next group member, and press enter to continue. ");
+		}
+		else
+		{
+			System.out.println("Now it is time for each group member to give their input. Pick one group member to begin.");
+			readLine("Press enter to continue.");
+		}
+	}
+	
+	private static void initialize()
+	{
+		System.out.println("Welcome to Decision Maker! This app will help you make decisions, as an individual or group.");
+		readLine("Press enter to continue. ");
+	}
+	
+	private static void getGroupDecision()
+	{
+		int[] totals = new int[users.size()];
 		
+		for(User user:users)
+		{
+			int counter = 0;
+			for(Alternative alt: user.getAlternatives())
+			{
+				totals[counter] = alt.getTotal();
+				counter++;
+			}
+		}
+		int highest = -1;
+		int highestIndex = -1;
+		for(int i = 0; i < totals.length; i++)
+		{
+			if(totals[i] > highest)
+			{
+				highest = totals[i];
+				highestIndex = i;
+			}
+		}
 		
-		
-		
+		System.out.println("Taking into account each person's input, the best group decision would be " + users.get(0).getAlternatives().get(highestIndex).getName());
+	}
+	
+	private static void makeUsers()
+	{
+		int input = readInt("Please enter the number of people in the group that is making the decision. If only you are making the decision, type \"1\": ");
+		for(int i = 0; i < input; i++)
+		{
+			User user = new User();
+			users.add(user);
+		}
 	}
 	
 	private static void getAlternatives()
 	{
-		System.out.println("Please input the name of one of the alternatives that you are considering.");
-		System.out.println("If you have inputted all of your alternatives already, type \"done\".");
+		space();
+		System.out.println("Please input the name of all of the alternatives that you are considering.");
+		if(users.size() > 1)
+		{
+			System.out.println("Please note that the entire group should agree on these alternatives.");
+		}
+		System.out.println("Once you have inputted all of your alternatives, type \"done\".");
 		int i = 1;
+		ArrayList<String> alternatives = new ArrayList<String>();
 		while(true)
 		{
-			System.out.println("Alternative " + i + ": ");
-			String input = readLine("");
+			String input = readLine("Alternative " + i + ": ");
 			
-			if(input.equals("done"))
+			if(input.equalsIgnoreCase("done"))
 			{
 				break;
 			}
 			
-			Alternative alt = new Alternative(input);
-			alternatives.add(alt);
+			alternatives.add(input);
 			i++;
+		}
+		for(User user: users)
+		{
+			for(String alt: alternatives)
+			{
+				user.addAlternative(alt);
+			}
 		}
 		
 	}
 	
-	private static void getCriteriaAndWeights()
+	private static void getCriteriaAndWeights(User user)
 	{
-		System.out.println("Now, please enter each of the criteria that you will use to evaluate the alternatives.");
+		space();
+		System.out.println("Please enter each of the criteria that you will use to evaluate the alternatives.");
 		System.out.println("After each criteria, enter a number from 1-100 to denote the importance of that criteria, with higher values corresponding to higher importance.");
 		System.out.println("Type \"done\" when you are finished");
 		while(true)
 		{
-			System.out.println("Criteria: ");
-			String input  = readLine("");
+			String input  = readLine("Criteria: ");
 			if(input.equals("done"))
 			{
 				break;
 			}
-			System.out.println("Weight: ");
-			Integer weight = readInt("");
-			criteria.put(input, weight);
+			int weight = 0;
+			while(true)
+			{
+				weight = readInt("Weight: ");
+				if(!(weight < 0 || weight > 100))
+				{
+					break;
+				}
+				System.out.println("Please enter a number between 1 and 100)");
+			}
+			
+			user.addCriteria(input, weight);
 		}
 	}
 	
-	private static void getAlternativeSuccess()
+	private static void getAlternativeSuccess(User user)
 	{
+		space();
 		System.out.println("Great! Now, you will need to evaluate how well each alternative fulfills the criteria.");
 		System.out.println("Enter a number from 1-100. A higher number should correspond to better fulfillment of the criteria.");
-		for(Alternative alternative: alternatives)
+		for(Alternative alternative: user.getAlternatives())
 		{
 			System.out.println("Alternative: " + alternative.getName());
-			for(String theCriteria: criteria.keySet())
+			for(String theCriteria: user.getCriteria().keySet())
 			{
-				System.out.println("Criteria: " + theCriteria);
-				int success = readInt("");
+				System.out.println("How well does the \"" + alternative.getName() + "\" alternative fulfill the \"" + theCriteria + "\" criteria? ");
+				int success;
+				while(true)
+				{
+					 success = readInt("");
+					 if(!(success < 0 || success > 100))
+					 {
+						  break;
+					 }
+					System.out.println("Please enter a number between 1 and 100)");
+				}
+				
 				alternative.addSuccess(success);
 			}
 		}
 	}
 	
-	private static void multiply()
+	private static void multiply(User user)
 	{
 		
-		for(Alternative alternative: alternatives)
+		for(Alternative alternative: user.getAlternatives())
 		{
 			int i = 0;
-			for(int critWeight: criteria.values())
+			for(int critWeight: user.getCriteria().values())
 			{
 				alternative.addToTotal(critWeight * alternative.getSuccess().get(i));
 				i++;
 			}
 		}
-		System.out.println("Great job. We have your results. Here are the point values for each alternative: ");
+		readLine("Great job. We have your results. Press enter to continue.");
+		System.out.println("Here are the point values for each alternative: ");
 		int highest = 0;
 		String highestAlt = "";
-		for(Alternative alternative: alternatives)
+		for(Alternative alternative: user.getAlternatives())
 		{
 			System.out.println(alternative.getName() + ": " + alternative.getTotal());
 			if(highest < alternative.getTotal())
@@ -101,8 +203,9 @@ public class Main {
 			}
 		}
 		System.out.println();
-		System.out.println("Based on these results, we recommend this alternative: ");
+		System.out.println("Based on these results, you would most likely prefer this alternative: ");
 		System.out.println(highestAlt);
+		readLine("Press enter to continue.");
 	}
 	public static String readLine(String prompt){
 		System.out.print(prompt);
@@ -116,9 +219,14 @@ public class Main {
 				int n = Integer.parseInt(input);
 				return n;
 			} catch (Exception e){
+				System.out.println("Please enter a number.");
 
 			}
 		}
+	}
+	public static void space()
+	{
+		System.out.println("---");
 	}
 	
 	
--- src/User.java
+++ src/User.java
@@ -1,4 +1,35 @@
+import java.util.ArrayList;
+import java.util.HashMap;
 
 public class User {
 
+	private static HashMap<String, Integer> criteria = new HashMap<String, Integer>();
+	private static ArrayList<Alternative> alternatives = new ArrayList<Alternative>();
+	
+	public User()
+	{
+		
+	}
+	
+	public ArrayList<Alternative> getAlternatives()
+	{
+		return alternatives;
+	}
+	
+	public void addAlternative(String name)
+	{
+		Alternative alternative = new Alternative(name);
+		alternatives.add(alternative);
+	}
+	
+	public HashMap<String, Integer> getCriteria()
+	{
+		return criteria;
+	}
+	
+	public void addCriteria(String name, int weight)
+	{
+		criteria.put(name, (Integer) weight);
+	}
 }
+
